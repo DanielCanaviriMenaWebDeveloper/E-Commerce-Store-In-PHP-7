ba423011ae9f5d87bc776391b82421775a4aea11
Daniel Alberto Canaviri Mena Web Developer <canavirimenadaniel@gmail.com>
2021-01-07 22:00:26 -0400

    E-Commerce Store In PHP 7 Version 0.1

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>E-comerce Store</title>
+        <!-- CSS - Bootstrap 3.3.7 -->
+        <link rel="stylesheet" type="text/css" href="styles/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="font-awesome/css/fontawesome.min.css">
+    </head>
+    <body>
+        <h1>Esta sera mi tienda de bootstrap</h1>
+
+
+        <script src="js/jquery.min.js"></script>
+        <!-- JS - Bootstrap 3.3.7 -->    
+        <script src="js/bootstrap.min.js"></script>
+    </body>
+</html>
